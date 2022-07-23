0a361b7674ac8986927bd5122da0c35ac7bffdc9
fabianofrank <fabianofrank@protonmail.com>
2022-07-23 18:29:16 -0300

    Fix views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,28 @@ class PostsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      flash[:notice] = 'Post created'
+    else
+      flash[:notice] = 'Could not create post'
+    end
+    redirect_to user_post_path(@current_user.id)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to: "users#index"
-  # get "/users", to: "users#index"
-  # get "/users/:id", to: "users#show"
-  # get "/users/:user_id/posts", to: "posts#index", as: :user_posts
-  # get "/users/:user_id/posts/:id", to: "posts#show", as: :user_post
+
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-   end
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:new, :create]
+    end
+  end
+
 end
