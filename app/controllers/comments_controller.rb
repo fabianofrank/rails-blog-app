64292a7fac24f3cf3ee2b3f57ef892bad208ee61
fabianofrank <fabianofrank@protonmail.com>
2022-07-24 07:14:37 -0300

    Fixing comments form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,22 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
   end
+
+  def create
+    @comment = Comment.new(comment_params) # title: params[:title], text: params[:text]
+    @comment.author_id = current_user.id
+    if @comment.valid?
+      @comment.save
+      flash[:notice] = 'Comment created'
+    else
+      flash[:notice] = 'Could not create comment'
+    end
+    redirect_to user_path(current_user.id)
+  end
+
+  private
+
+  def comment_params
+    params.permit(:text)
+  end
 end
